4bb5a2e637d77121986e6664f87d077a28a941ac
Jirasak Buranathawornsom <wanjirasak@gmail.com>
2021-05-09 22:24:00 +0700

    Add all_checks file to health_checks

---
+++ all_checks.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+def check_reboot():
+    """Return True if the computer has a pending reboot."""
+    return os.path.exists("/run/reboot-required")
+
+def main():
+    if check_reboot():
+        print("Pending Reboot.")
+        sys.exit(1)
+    print("Everything ok.")
+    sys.exit(0)
+main()
