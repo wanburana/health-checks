98b06cb72de764eadffb58a6333b6502f1804363
22p22w0072 <wanjirasak@gmail.com>
2021-05-09 22:48:01 +0700

    reorder boolean condition to match the argument

--- all_checks.py
+++ all_checks.py
@@ -13,7 +13,7 @@ def check_disk_full(disk, min_gb, min_percent):
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_gb:
+    if gigabyte_free > min_absolute or percent_free < min_percent:
         return True
     return False
     
